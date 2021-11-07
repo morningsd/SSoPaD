b8e7d8035f5146f19a07644f7c7992575f9d3a53
Oleksii Krasnovskyi <finchatticu33@gmail.com>
2021-11-07 11:47:38 +0200

    fix path, user can't read admin files

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,15 +6,22 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <ftw.h>
+#include <fstream>
 
 using namespace std;
 
-#define CHANGEFILE "/home/demian/study/ss/lab1/mydir"
-#define USER_PERMISSIONS S_IRUSR|S_IXUSR // rx
-#define ADMIN_PERMISSIONS S_IRWXU // rwx
+#define WORKING_DIRECTORY "/home/demian/study/ss/lab1" // path from which you start the program
+#define CHANGEFILE "/home/demian/study/ss/lab1/mydir"// working directory + name of the directory
+//#define USER_PERMISSIONS S_IRUSR|S_IXUSR // rx
+//#define ADMIN_PERMISSIONS S_IRWXU // rwx
+#define PERMISSIONS S_IRWXU //rwx
+#define AUTH_FILE "/home/demian/study/ss/lab1/.auth" // file with metadata
 
 void list_dir(const char *dir,int op_a,int op_l);
 int rmrf(char *path);
+int create_file(string path, string role);
+int read_file(string path, string role);
+
 
 int main()
 {
@@ -27,11 +34,11 @@ int main()
     cin >> login;
       
     if (login == "user") {
-      chmod(CHANGEFILE, USER_PERMISSIONS);
+      //chmod(CHANGEFILE, USER_PERMISSIONS);
       logged = true;
       break;
     } else if (login == "admin") {
-      chmod(CHANGEFILE, ADMIN_PERMISSIONS);
+      //chmod(CHANGEFILE, ADMIN_PERMISSIONS);
       logged = true;
       break;
     } else {
@@ -40,6 +47,8 @@ int main()
       
   } while (!logged);
   cout << "You are successfully logged in as " << login << "\n";
+  chmod(CHANGEFILE, PERMISSIONS);
+  chdir(CHANGEFILE);
   
   while (true) {
     char command[255];
@@ -53,8 +62,16 @@ int main()
     if (strcmp(command, "cd") == 0) {
       cin >> param;
       cout << param << "\n";
+      char new_cwd[1024];
       if (chdir(param) != 0) {
 	cout << "Can't change directory to " << param << "\n";
+      }
+      getcwd(new_cwd, sizeof(new_cwd));
+      string new_cwd_str(new_cwd);
+      if (!(new_cwd_str.rfind(CHANGEFILE, 0) == 0)) {
+	cout << "Can't exit current working directory\n";
+	chdir(cwd);
+	continue;
       }
     } else if (strcmp(command, "rm") == 0) {
       cin >> param;
@@ -73,22 +90,23 @@ int main()
 	if (mkdir(param, 0) != 0) {
 	  cout << "Can't make directory" << "\n";
 	} else if (login == "admin") {
-	  chmod(param, ADMIN_PERMISSIONS);
+	  //chmod(param, ADMIN_PERMISSIONS);
 	}
       }
     } else if (strcmp(command, "vi") == 0) {
       cin >> param;
       string command_str;
-      if (login == "admin") {
-	command_str = string(command);
-      } else {
-	command_str = "view"; 
-      }
+      command_str = string(command);
+     
       string param_str = string(param);
       string concat = command_str + " " + param_str;
       char result[concat.size() + 1];
       strcpy(result, concat.c_str());
-      system(result);
+      if (create_file(string(cwd) + "/" + param_str, login) == 0) {
+	system(result);
+      } else {
+	cout << "Can't call vi " << param_str << "\n";
+      }
     } else {
       cout << "Unknown command.\n";
     }
@@ -119,3 +137,50 @@ void list_dir(const char *dir,int op_a,int op_l) {
   if (!op_l) printf("\n");
 }
 
+int create_file(string path, string role) {
+  int res = read_file(path, role);
+  if (res == 1) {
+    cout << "Success\n";
+    return 0;
+  } else if (res == -1) {
+    cout << "User " << role << " have no access to file: " << path << "\n";
+    return -1;
+  }
+  
+  ofstream fs(AUTH_FILE, ios_base::app);
+
+  if (!fs) {
+    cout << "Cannot open the output file.\n";
+    return -1;
+  }
+  
+  fs << role << " " << path << "\n";
+  
+  fs.close();
+
+  return 0;
+}
+
+int read_file(string path, string role) {
+  cout << "path: " << path + " role: " << role << "\n";
+  fstream fs(AUTH_FILE, ios::in);
+  if (!fs.is_open()) {
+    return -2;
+  }
+  string check_file = role + " " + path;
+  string auth_role;
+  string auth_path;
+  while(fs) {
+    fs >> auth_role >> auth_path;
+    //cout << "auth_role: " << auth_role << "\n";
+    //cout << "auth_path: " << auth_path << "\n";
+    if (auth_path == path) {
+      if ((role == "admin") || (auth_role == role)) {
+	return 1;
+      } else {
+	return -1;
+      }
+    }
+  }
+  return 0;
+}
